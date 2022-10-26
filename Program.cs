64125faa6fe6015aaaf9543ba5aa0038900688f2
KatiaRassadina <rassadinaekaterina877@gmail.com>
2022-10-26 22:54:26 +0300

    task done

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// Цикл принимает на вход два числа (A и B) и возводит число A в натуральную степень B
+Console.WriteLine("Input an integer number A");
+int A=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input an integer number B");
+int B=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Math.Pow(A, B));
